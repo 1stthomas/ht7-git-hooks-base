74f9160bfbd921c9bccd408c8af4fd996fa20839
1stthomas <thomas.pluess@students.ffhs.ch>
2025-04-16 18:46:13 +0200

    #1 add function for concatenated tracked and untracked files

--- src/functions/git.sh
+++ src/functions/git.sh
@@ -6,7 +6,20 @@ get_diff_files() {
     echo "$diff_files"
 }
 
-get_diff_untracked_files() {
+get_diff_files_all() {
+    local files_tracked
+    files_tracked=$(get_diff_files)
+    local files_untracked
+    files_untracked=$(get_diff_files_untracked)
+    local separator
+    [ -n "$files_tracked" ] && [ -n "$files_untracked" ] && separator="\n" || separator=""
+    local files_all
+    files_all=$(printf "%s%s%s" $files_tracked $separator $files_untracked)
+
+    echo "$files_all"
+}
+
+get_diff_files_untracked() {
     local untracked_files
     # src: https://stackoverflow.com/questions/3801321/git-list-only-untracked-files-also-custom-commands
     untracked_files=$(git ls-files --others --exclude-standard)
