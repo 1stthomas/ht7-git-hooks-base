5d3dbb5bce842b1934f14a8d645374179c8c2341
1stthomas <thomas.pluess@students.ffhs.ch>
2025-04-23 18:37:36 +0200

    #1 simplify git functions

--- src/functions/git.sh
+++ src/functions/git.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 get_diff_files() {
-    local diff_files
-    diff_files=$(git diff --name-only HEAD)
-    echo "$diff_files"
+    echo $(git diff --name-only HEAD)
 }
 
 get_diff_files_all() {
@@ -12,16 +10,12 @@ get_diff_files_all() {
     local files_untracked
     files_untracked=$(get_diff_files_untracked)
     local separator
-    [ -n "$files_tracked" ] && [ -n "$files_untracked" ] && separator="\n" || separator=""
-    local files_all
-    files_all=$(printf "%s%s%s" "$files_tracked" "$separator" "$files_untracked")
-
-    echo "$files_all"
+    [ -n "$files_tracked" ] && [ -n "$files_untracked" ] && separator=" " || separator=""
+    
+    printf "%s%s%s" "$files_tracked" "$separator" "$files_untracked"
 }
 
 get_diff_files_untracked() {
-    local untracked_files
     # src: https://stackoverflow.com/questions/3801321/git-list-only-untracked-files-also-custom-commands
-    untracked_files=$(git ls-files --others --exclude-standard)
-    echo "$untracked_files"
+    echo $(git ls-files --others --exclude-standard)
 }
