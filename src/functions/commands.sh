03e7fe1df81743152d25156e297957cf861fd1ad
1stthomas <thomas.pluess@students.ffhs.ch>
2025-04-30 18:27:56 +0200

    #1 fix command check

--- src/functions/commands.sh
+++ src/functions/commands.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 check_commands() {
-    local commands=("php" "phpstan" "php-cs-fixer" "phpunit")
+    local commands=("php" "vendor/bin/phpstan" "vendor/bin/php-cs-fixer" "vendor/bin/phpunit")
+    path_base=$(git rev-parse --show-toplevel)
+    
     for cmd in "${commands[@]}"; do
-        if ! command -v "$cmd" &> /dev/null; then
-            echo "$cmd could not be found. Please install it."
+        [[ "$cmd" == *"/"* ]] && path_cmd="$path_base/$cmd" || path_cmd="$cmd"
+        if ! command -v "$path_cmd" &> /dev/null; then
+            echo "$cmd could not be found ($path_cmd). Please install it."
             # exit 1
         fi
     done
