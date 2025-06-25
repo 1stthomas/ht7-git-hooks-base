c060351eb0c1b0167fe3435df4a7f8657537e2ac
1stthomas <thomas.pluess@students.ffhs.ch>
2025-06-25 19:53:40 +0200

    #1 make sure the expected function is called

--- src/functions/initialization.sh
+++ src/functions/initialization.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-is_installed() {
-    local path_root=$(get_root_path_repository)
-    
-    if [[ -d "$path_root/.git/hooks/ht7" ]]; then
-        return 0 # Installed
+is_installed_old() {
+  local path_root=$(get_root_path_repository)
+  
+  if [[ -d "$path_root/.git/hooks/ht7" ]]; then
+    if [[ -f "$repo_root/somef.ht7git" ]]; then
+      return 0 # Installed
     else
-        return 1
+      return 1
     fi
+  else
+    return 1
+  fi
 }
